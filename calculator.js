e9548c3cbbb04d318005c33c418be524b88ee787
Noor Fatima <161709158+FatimaDevloper@users.noreply.github.com>
2025-04-19 22:48:50 +0500

    calculator
    Signed-off-by: Noor Fatima <161709158+FatimaDevloper@users.noreply.github.com>

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function appendValue(value){
+     document.getElementById("result").value += value;
+}
+function clearResult(){
+    document.getElementById("result")  .value=""; 
+}
+function calculator(){
+    try{
+        let result=
+        eval(document.getElementById("result").value);
+        document.getElementById("result").value=result;
+        } catch{
+            document.getElementById( "result").value="Error"
+        }
+}
